938d75287f9b6741beed35d1784dd2c9aaf1b03d
guilhermeazis <guilhermeazis1@gmail.com>
2023-04-18 19:46:34 -0300

    Finalidado função e iniciado objeto

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objecto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objecto.processamento()
+
+//Principios importantes
+//1. Abstração
+//2. Encapsulamento
+//3. Herança (prototype)
+//4. Polimorfismo
