11c7405acd040642c979b0c9e34da82efff519e8
guilhermeazis <guilhermeazis1@gmail.com>
2023-04-18 19:45:53 -0300

    Finalidado função e iniciado objeto

--- exercicios-js/funcao/funcoesAnonimas.js
+++ exercicios-js/funcao/funcoesAnonimas.js
@@ -9,7 +9,7 @@ const imprimirResultado = function (a, b, operacao = soma){
 imprimirResultado(3, 4)
 imprimirResultado(3, 4, soma)
 imprimirResultado(3, 4, function (x, y){
-    return x- y
+    return x - y
 })
 imprimirResultado(3, 4, (x, y) => x * y)
 
