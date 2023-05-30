22c9a428ab599cdb530c221419f7183bcb786c3b
guilhermeazis <guilhermeazis1@gmail.com>
2023-05-30 19:04:22 -0300

    Concluido o capítulo de node e esnext

---
+++ exercicios-js/node/global.js
@@ -0,0 +1,7 @@
+// console.log(global)
+globalThis.MinhaApp = Object.freeze({
+    saudacao() {
+        return 'Estou em todods os lugares!'
+    },
+    nome: 'Sistema Legal'
+})
---
+++ exercicios-js/node/usandoModulosTerceiros.js
@@ -0,0 +1,2 @@
+const _ = require('loadsh')
+setInterval(() => console.log(_.random(500, 600)), 2000)
