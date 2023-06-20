a64a9a7d2d4c239fd45eded9a8a7a80f45fe1d21
guilhermeazis <guilhermeazis1@gmail.com>
2023-06-20 11:29:37 -0300

    Fim do capítulo de ajax e gulp

---
+++ exercicios-js/gulp/basico/gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+/* const series = gulp.series */
+
+const antes1 = callback => {
+    console.log('Tarefa Antes1!')
+    return callback()
+}
+
+const antes2 = callback => {
+    console.log('Tarefa Antes2!')
+    return callback()
+}
+
+function copiar(callback) {
+    /* gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) */
+
+    gulp.src('pastaA/**/*.txt')
+        /* .pipe(imagePelaMetade())
+        .pipe(imagemEmPretoEBranco())
+        .pipe(transformacaoA())
+        .pipe(transformacaoB())
+        .pipe(transformacaoC()) */
+        .pipe(gulp.dest('pastaB'))
+    return callback()
+}
+
+const fim = callback => {
+    console.log('Tarefa Fim!')
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
